c9916fb726c97e1580fa979a000b28ec8a9f6933
masashi <mss.nt.2828@gmail.com>
2022-04-22 19:57:30 +0900

    タブの追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
-      <v-img
-        :src="require('../assets/logo.svg')"
-        class="my-3"
-        contain
-        height="200"
-      />
+      <template>
+        <v-card class="mx-auto" max-width="300" tile>
+          <v-list flat>
+            <v-subheader>Tab</v-subheader>
+            <v-list-item-group v-model="selectedItem" color="primary">
+              <v-list-item v-for="(item, i) in items" :key="i">
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </template>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -30,6 +40,19 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    selectedItem: 1,
+    items: [
+      { text: "Account", icon: "mdi-account" },
+      { text: "Test0", icon: "mdi-flag" },
+      { text: "Test1", icon: "mdi-flag" },
+      { text: "Test2", icon: "mdi-flag" },
+      { text: "Test3", icon: "mdi-flag" },
+      { text: "Test4", icon: "mdi-flag" },
+      { text: "Test5", icon: "mdi-flag" },
+      { text: "Test6", icon: "mdi-flag" },
+    ],
+  }),
 };
 </script>
