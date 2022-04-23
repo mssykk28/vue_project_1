b2c81947b79a39d205d8f462e317664d49d585f6
masashi <mss.nt.2828@gmail.com>
2022-04-23 16:59:48 +0900

    コンポーネント分ける

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,8 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <template>
-        <v-card class="mx-auto" max-width="300" tile>
-          <v-list flat>
-            <v-subheader>Tab</v-subheader>
-            <v-list-item-group v-model="selectedItem" color="primary">
-              <v-list-item v-for="(item, i) in items" :key="i">
-                <v-list-item-icon>
-                  <v-icon v-text="item.icon"></v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.text"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-card>
-      </template>
-    </v-navigation-drawer>
+      <MenuItem
+    /></v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -39,20 +23,14 @@
 </template>
 
 <script>
+import MenuItem from "./MenuItem";
+
 export default {
   data: () => ({
     drawer: null,
-    selectedItem: 1,
-    items: [
-      { text: "Account", icon: "mdi-account" },
-      { text: "Test0", icon: "mdi-flag" },
-      { text: "Test1", icon: "mdi-flag" },
-      { text: "Test2", icon: "mdi-flag" },
-      { text: "Test3", icon: "mdi-flag" },
-      { text: "Test4", icon: "mdi-flag" },
-      { text: "Test5", icon: "mdi-flag" },
-      { text: "Test6", icon: "mdi-flag" },
-    ],
   }),
+  components: {
+    MenuItem,
+  },
 };
 </script>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="mx-auto" max-width="300" tile>
+    <v-list flat>
+      <v-subheader>Tab</v-subheader>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectedItem: 1,
+    items: [
+      { text: "Account", icon: "mdi-account" },
+      { text: "Test0", icon: "mdi-flag" },
+      { text: "Test1", icon: "mdi-flag" },
+      { text: "Test2", icon: "mdi-flag" },
+      { text: "Test3", icon: "mdi-flag" },
+      { text: "Test4", icon: "mdi-flag" },
+      { text: "Test5", icon: "mdi-flag" },
+      { text: "Test6", icon: "mdi-flag" },
+    ],
+  }),
+};
+</script>
