cd45c7ab205ee1b90b5cd7638c89bf9c78cbc9fb
masashi <mss.nt.2828@gmail.com>
2022-04-29 15:18:03 +0900

    MenuItem追加

--- src/components/WireFrame.vue
+++ src/components/WireFrame.vue
@@ -1,31 +1,26 @@
 <template>
-  <v-app id="inspire">
-    <header>
-      <v-app-bar app dark>
-        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>MySuite</v-toolbar-title>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" fixed temporary>
-        <v-list nav dense>
-          <v-list-item-group>
-            <MenuItem>
-              <template v-slot:icon> mdi-home </template>
-              <template v-slot:bodyText> Home </template>
-            </MenuItem>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-    </header>
+  <header>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>MySuite</v-toolbar-title>
+    </v-app-bar>
 
-    <v-main>
-      <v-img
-        :src="require('../assets/logo.svg')"
-        class="my-3"
-        contain
-        height="200"
-      />
-    </v-main>
-  </v-app>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list nav>
+        <v-list-item-group>
+          <MenuItem>
+            <template v-slot:icon> mdi-home </template>
+            <template v-slot:bodyText> Home </template>
+          </MenuItem>
+
+          <MenuItem>
+            <template v-slot:icon> mdi-account </template>
+            <template v-slot:bodyText> Account </template>
+          </MenuItem>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
 </template>
 
 <script>
