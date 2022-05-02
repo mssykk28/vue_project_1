d9db6242e4a3f69891c221ff395533b9f2ce2abb
masashi <mss.nt.2828@gmail.com>
2022-05-02 22:54:26 +0900

    Footerのコンポーネント

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,7 @@
     <v-main>
       <router-view></router-view>
     </v-main>
-    <template>
-      <v-footer dark padless>
-        <v-row justify="center" no-gutters>
-          <v-btn
-            v-for="link in links"
-            :key="link"
-            color="white"
-            text
-            rounded
-            class="my-2"
-          >
-            {{ link }}
-          </v-btn>
-          <v-col class="py-4 text-center white--text" cols="12">
-            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-          </v-col>
-        </v-row>
-      </v-footer>
-    </template>
+    <Footer />
   </v-app>
 </template>
 
@@ -30,15 +12,15 @@
 import Vue from "vue";
 
 import WireFrame from "@/components/WireFrame.vue";
+import Footer from "@/components/Footer.vue";
 
 export default Vue.extend({
   name: "App",
 
-  data: () => ({
-    links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"],
-  }),
+  data: () => ({}),
   components: {
     WireFrame,
+    Footer,
   },
 });
 </script>
